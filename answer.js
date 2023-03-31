17ecbe2172fd566c81489fa9dc778b83c2fc9792
Kalash garg <86044442+KalashGarg@users.noreply.github.com>
2023-03-31 16:34:16 +0530

    Add files via upload

---
+++ answer.js
@@ -0,0 +1,8 @@
+var num1, num2;
+num1 = window.prompt("Input the First integer", "0");
+num2 = window.prompt("Input the second integer", "0");
+if(num1%num2==0){
+    document.write("first number is multiple of second number ");
+}else{
+    document.write("first number is not multiple of second number ");
+}
